538bf5344c26052888079f650ca57cee6053091c
Pixel1011 <enderdragon1105@gmail.com>
2025-07-16 18:50:51 +0100

    stop processes when uninstalling + revert info.txt to have correct default log path

--- dont-touch/uninstall.ps1
+++ dont-touch/uninstall.ps1
@@ -12,6 +12,11 @@ $taskName = "WiiWallpaper"
 schtasks /Query /TN $taskName > $null 2>&1
 if ($LASTEXITCODE -eq 0) {
     Write-Output "Uninstalling..."
+    Write-Output "Ending WiiWallpaper task.."
+    schtasks /End /TN "WiiWallpaper"
+    Write-Output "Killing wallpaper process.."
+    taskkill /IM lillywallpaper.exe /F
+    Write-Output "Removing WiiWallpaper task.."
     schtasks /Delete /TN $taskName /F
 } else {
     Write-Output "Task '$taskName' is not installed!"
